c6db3904832c36f05f098faa5abf978c8cde11e3
Michael Matasyants <michael.matasyanc@gmail.com>
2023-09-24 16:34:06 +0300

    Fix view naming

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -24,5 +24,5 @@ from places import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.show_main_page),
-    path('places/<int:place_id>/', views.place_view),
+    path('places/<int:place_id>/', views.show_place_json),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
