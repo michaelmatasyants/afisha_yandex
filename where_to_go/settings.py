8e20faaccda3be1c6383ef45b43145738f361711
Michael Matasyants <michael.matasyanc@gmail.com>
2023-10-19 15:31:06 +0300

    Add README.md

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default='127.0.0.1')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1'])
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 SECURE_HSTS_SECONDS = 31536000
