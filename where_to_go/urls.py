3b7fbbe49a8444b0ebb4e59aa2da83940874c586
Michael Matasyants <michael.matasyanc@gmail.com>
2023-11-04 19:13:59 +0300

    Sort imports

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,7 +1,8 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+
 from places import views
 
 
